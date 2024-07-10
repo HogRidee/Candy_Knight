4dec3d585a7ea84919120e3ab794da99f9b11f86
HogRidee <iaraoz061102@gmail.com>
2024-07-10 13:44:58 -0500

    Add flip to the player

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,6 +7,7 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -20,6 +21,13 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	# Flip the player where its looking
+	if direction > 0:
+		animated_sprite.flip_h = false;
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
