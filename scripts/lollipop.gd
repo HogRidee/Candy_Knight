054be096232fe87b79378fdfc2ca40c336da5ef1
HogRidee <iaraoz061102@gmail.com>
2024-07-12 13:33:34 -0500

    Fix enemy cliff detection

--- scripts/lollipop.gd
+++ scripts/lollipop.gd
@@ -7,13 +7,23 @@ var direction = 1
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var ray_cast_bottom = $RayCastBottom
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# Detect lateral collisions
 	if ray_cast_right.is_colliding():
 		direction = -1
 		animated_sprite.flip_h = false
 	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = true
+	
+	# Detect cliffs
+	if not ray_cast_bottom.is_colliding() and direction == 1:
+		direction = -1
+		animated_sprite.flip_h = false
+	if not ray_cast_bottom.is_colliding() and direction == -1:
 		direction = 1
 		animated_sprite.flip_h = true
 	
