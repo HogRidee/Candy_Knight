82641244bea25779e6f09aa2c80f5ba6462c6e12
HogRidee <iaraoz061102@gmail.com>
2024-07-12 11:29:13 -0500

    Add enemy/playear death animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,10 +6,13 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var is_dead = false
+
 # Get the reference from the animated sprite node
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var collision_shape_2d = $CollisionShape2D
 
-func _physics_process(delta):
+func _physics_process(delta): 
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -28,15 +31,14 @@ func _physics_process(delta):
 		animated_sprite.flip_h = true
 	
 	# Play animations.
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle")
+	if not is_dead:
+		if is_on_floor():
+			if direction == 0:
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("run")
 		else:
-			animated_sprite.play("run")
-	else:
-		animated_sprite.play("jump_v3")
-	
-	
+			animated_sprite.play("jump")
 	
 	# Handle the movement/deceleration.
 	if direction:
@@ -46,4 +48,8 @@ func _physics_process(delta):
 
 	move_and_slide()
 
-
+# Handle player death
+func die():
+	is_dead = true
+	animated_sprite.play("damage")
+	
