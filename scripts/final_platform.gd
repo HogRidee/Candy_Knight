308451833a9a1b25779f8618e514091abe07f904
HogRidee <iaraoz061102@gmail.com>
2024-07-13 11:16:21 -0500

    Change camera in final level

--- scripts/final_platform.gd
+++ scripts/final_platform.gd
@@ -2,6 +2,7 @@ extends AnimatableBody2D
 
 @onready var animation_player = $AnimationPlayer
 @onready var timer = $Timer
+@onready var camera = %Camera2D
 
 var first_time = true
 
@@ -10,7 +11,13 @@ func _process(delta):
 	if Global.stop_platform and first_time:
 		timer.start()
 		first_time = false
+		adjust_camera()
 		
-
 func _on_timer_timeout():
 	animation_player.pause()
+
+func adjust_camera():
+	var camera_offset = Vector2(0,-51)
+	camera.position += camera_offset
+	camera.limit_left -= 100
+	camera.limit_right += 100
