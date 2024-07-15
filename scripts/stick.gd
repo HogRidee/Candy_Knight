f9ae6cdc70619fb7e72f6752d50dc0d1b96a2412
HogRidee <iaraoz061102@gmail.com>
2024-07-15 04:07:44 -0500

    Fix stick collisions

--- scripts/stick.gd
+++ scripts/stick.gd
@@ -7,7 +7,6 @@ var direction = 1
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
-@onready var ray_cast_bottom = $RayCastBottom
 
 func _process(delta):
 	# Detect lateral collisions
@@ -15,14 +14,6 @@ func _process(delta):
 		direction = -1
 		animated_sprite.flip_h = true
 	if ray_cast_left.is_colliding():
-		direction = 1
-		animated_sprite.flip_h = false
-	
-	# Detect cliffs
-	if not ray_cast_bottom.is_colliding() and direction == 1:
-		direction = -1
-		animated_sprite.flip_h = true
-	if not ray_cast_bottom.is_colliding() and direction == -1:
 		direction = 1
 		animated_sprite.flip_h = false
 	
