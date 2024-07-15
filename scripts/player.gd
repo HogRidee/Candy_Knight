ce442eb7c2f36b221296d8448fe6ed6ddbff7b0d
HogRidee <iaraoz061102@gmail.com>
2024-07-15 08:59:13 -0500

    Add attack animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,6 +7,7 @@ const JUMP_VELOCITY = -310.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var is_dead = false
+var is_attacking = false
 
 # Get the reference from the animated sprite node
 @onready var animated_sprite = $AnimatedSprite2D
@@ -22,6 +23,11 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	# Handle attack
+	if Input.is_action_just_pressed("attack") and not is_dead:
+		is_attacking = true
+		animated_sprite.play("attack")
+
 	# Get the input direction: -1 0 1
 	var direction = Input.get_axis("move_left", "move_right")
 	
@@ -32,7 +38,7 @@ func _physics_process(delta):
 		animated_sprite.flip_h = true
 	
 	# Play animations.
-	if not is_dead:
+	if not is_dead and not is_attacking:
 		if is_on_floor():
 			if direction == 0:
 				animated_sprite.play("idle")
@@ -53,3 +59,8 @@ func _physics_process(delta):
 func die():
 	is_dead = true
 	animated_sprite.play("damage")
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if animated_sprite.animation == "attack":
+		is_attacking = false
